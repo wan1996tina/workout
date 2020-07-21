f08afa77370f020ced47cc7bceae6fe221fe6c02
wan1996tina <gana7138@gmail.com>
2020-07-21 16:37:48 +0800

    關於頁banner

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './style/style.styl'
-
+import animated from 'animate.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -16,9 +18,9 @@ library.add(faCoffee, faHome, faDumbbell, faInfoCircle, faStopwatch)
 
 // 註冊元件
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
+Vue.use(animated)
 Vue.config.productionTip = false
-
+AOS.init()
 new Vue({
   router,
   store,
--- src/views/Information.vue
+++ src/views/Information.vue
@@ -2,9 +2,18 @@
   #information(ref='info')
     b-container(fluid style='width:95%;')
       b-col.toolbar(cols='12')
-        p.subtitle Information
-      b-col.banner.my-2(cols='11')
+        p.subtitle.d-none Information
+      b-col.banner(cols='11')
         img.woman(src='~@/assets/woman.png')
-        h2.info-title Fast. Convenient. Affordable.
-
+        h2.info-subtitle Fast.&emsp;Convenient.&emsp;Affordable.
+        h2.info-title(class="animate__animated animate__fadeInDown") RELAX
+        h2.info-back-title (yourself)
+      b-container.text-center(style='width:95%;')
+        b-row(class="animate__animated  animate__infinite")
+          b-col.more(cols="12") More about us
+          div.triangle.mx-auto.mt-2
+      b-container.content1
+        b-col(cols='4')
+          div.num1
+          div.text-center.mx-auto.c1-title purpose
 </template>
